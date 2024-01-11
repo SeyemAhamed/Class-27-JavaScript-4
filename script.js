c3cd1e289bfaacfa9d962d3964a63654d122c305
SeyemAhamed <seyamjh@gmail.com>
2024-01-11 21:48:54 +0600

    Array in Javascript

---
+++ script.js
@@ -0,0 +1,29 @@
+
+// ===Index Array===
+
+let marks =[40, 50, 75, 35, 21];
+
+
+console.log(marks[4]);
+
+
+// ===Tow Dimensional Array===
+
+
+let towDimensionalArray = [ //Camel Case
+  ['Rahim','Karim','Bablu','Abdul','Jobbar'],
+  ['Rubel','Shahin','Arif','Seyem'],
+  ['Utpol','Manik','Maruf','Mou'],
+]
+
+
+console.log (towDimensionalArray[2][2]);
+console.log (towDimensionalArray[1][3]);
+
+//Associative Array.....
+
+let Seyem = {
+    firstName:'karim',age: 23, accoutBalance: 123215
+}
+
+console.log (Seyem.age);
